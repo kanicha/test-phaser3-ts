6882a3ae76067b0876790642fa1cc86b51d6a8ae
yu.fukai <yu.fukai@nobollel.com>
2025-08-20 18:35:19 +0900

    update scripts

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,63 @@
-export {}; // ← これ必要！
+import type PhaserType from 'phaser'; // ← 型定義のためのimport（JSには出力されない）
+declare const Phaser: typeof PhaserType; // ← 実体はCDNで提供されるグローバル変数
 
-import Phaser from 'phaser';
+class MyGame extends Phaser.Scene {
+    private logo!: Phaser.Physics.Arcade.Image;
 
-class MyScene extends Phaser.Scene {
-  constructor() {
-    super('main');
-  }
+    constructor() {
+        super("mygame");
+    }
 
-  create() {
-    this.add.text(100, 100, 'Chrome 66 OK!', { font: '24px Arial', color: '#ffffff' });
-  }
+    preload() {
+        console.log("GAME >> Preload Start");
+        this.load.setBaseURL("https://labs.phaser.io");
+        this.load.image("sky", "assets/skies/space3.png");
+        this.load.image("logo", "assets/sprites/phaser3-logo.png");
+
+        this.load.audio("pop", "assets/audio/SoundEffects/p-ping.mp3");
+
+        console.log("GAME >> Preload Ended");
+    }
+
+    create() {
+        console.log("GAME >> Create Start");
+
+        this.add.image(400, 300, "sky");
+
+        this.spawnLogo(400, 100)
+
+        // クリックイベントの登録
+        this.input.on("pointerdown", (pointer: Phaser.Input.Pointer) => {
+            this.spawnLogo(pointer.x, pointer.y);
+        });
+
+        console.log("GAME >> Create Ended");
+    }
+
+    spawnLogo(x: number, y: number) {
+        const logo = this.physics.add.image(x, y, "logo");
+        logo.setVelocity(Phaser.Math.Between(-200, 200), Phaser.Math.Between(-200, 200));
+        logo.setBounce(1, 1);
+        logo.setCollideWorldBounds(true);
+
+        this.sound.play("pop", { volume: 0.1 });
+    }
+
+    update() {}
 }
 
-new Phaser.Game({
-  type: Phaser.CANVAS,
-  width: 800,
-  height: 600,
-  scene: MyScene,
-  backgroundColor: '#000'
-});
+const config: Phaser.Types.Core.GameConfig = {
+    type: Phaser.CANVAS,
+    width: 800,
+    height: 600,
+    physics: {
+        default: "arcade",
+        arcade: { gravity: {
+                y: 200,
+                x: 0
+            } }
+    },
+    scene: MyGame
+};
+
+new Phaser.Game(config);
