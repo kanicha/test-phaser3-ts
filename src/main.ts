1502caabe76286814f7822c4dc2b3843c94b8503
SakamakiDaiki <daiki.sakamaki@nobollel.com>
2025-08-20 12:26:53 +0900

    click event and sounds

--- src/main.ts
+++ src/main.ts
@@ -11,15 +11,28 @@ class MyGame extends Phaser.Scene {
         this.load.setBaseURL("https://labs.phaser.io");
         this.load.image("sky", "assets/skies/space3.png");
         this.load.image("logo", "assets/sprites/phaser3-logo.png");
+
+        this.load.audio("pop", "assets/audio/SoundEffects/p-ping.mp3");
     }
 
     create() {
         this.add.image(400, 300, "sky");
 
-        this.logo = this.physics.add.image(400, 100, "logo");
-        this.logo.setVelocity(200, 200);
-        this.logo.setBounce(1, 1);
-        this.logo.setCollideWorldBounds(true);
+        this.spawnLogo(400, 100)
+
+        // クリックイベントの登録
+        this.input.on("pointerdown", (pointer: Phaser.Input.Pointer) => {
+            this.spawnLogo(pointer.x, pointer.y);
+        });
+    }
+
+    spawnLogo(x: number, y: number) {
+        const logo = this.physics.add.image(x, y, "logo");
+        logo.setVelocity(Phaser.Math.Between(-200, 200), Phaser.Math.Between(-200, 200));
+        logo.setBounce(1, 1);
+        logo.setCollideWorldBounds(true);
+
+        this.sound.play("pop");
     }
 
     update() {}
@@ -27,8 +40,8 @@ class MyGame extends Phaser.Scene {
 
 const config: Phaser.Types.Core.GameConfig = {
     type: Phaser.AUTO,
-    width: 800,
-    height: 600,
+    width: 1080,
+    height: 1920,
     physics: {
         default: "arcade",
         arcade: { gravity: {
